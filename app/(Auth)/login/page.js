a91e1878019d9cee829886f1c262f51db6d04741
frustrated018 <niloyd018@gmail.com>
2024-02-19 18:26:48 +0600

    Did the basic layout for login and signup pages

--- app/(Auth)/login/page.js
+++ app/(Auth)/login/page.js
@@ -1,9 +1,34 @@
+import { ArrowTopRightIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+
 const LoginPage = () => {
-    return (
-        <div className='min-h-screen grid place-items-center'>
-            <h2 className='text-4xl text-green-500 font-semibold'>Login</h2>
+  return (
+    <main className="min-h-screen">
+      <section className="w-full flex">
+        {/* //! left section */}
+        <div className="w-1/2 bg-secondary h-screen p-6 flex flex-col justify-between">
+          <Link href="/" className="text-4xl font-semibold">
+            GOGO Rides
+          </Link>
+          <p className="text-lg">
+            Rev up your sales with our one-stop shop for all vehicles! Sell
+            anything on wheels effortlessly and profitably today!
+          </p>
         </div>
-    );
+        {/* //! Right section */}
+        <div className="w-1/2 h-screen grid place-items-center">
+          <h2 className="text-4xl">Login Page</h2>
+          <h2 className="text-4xl"> this is where the form will be</h2>
+          <p className="inline-flex gap-1 text-base">
+            Don&apos;t have an account?{" "}
+            <Link href="/signup" className="hover:underline flex items-center">
+              signup <ArrowTopRightIcon />
+            </Link>
+          </p>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default LoginPage;
--- app/(Auth)/signup/page.js
+++ app/(Auth)/signup/page.js
@@ -1,10 +1,35 @@
+import { ArrowTopRightIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 const SignupPage = () => {
-    return (
-        <div className='min-h-screen grid place-items-center'>
-            <h2 className='text-4xl text-green-500 font-semibold'>Signup</h2>
+  return (
+    <main className="min-h-screen">
+      <section className="w-full flex">
+        {/* //! left section */}
+        <div className="w-1/2 bg-secondary h-screen p-6 flex flex-col justify-between">
+          <Link href="/" className="text-4xl font-semibold">
+            GOGO Rides
+          </Link>
+          <p className="text-lg">
+            Rev up your sales with our one-stop shop for all vehicles! Sell
+            anything on wheels effortlessly and profitably today!
+          </p>
         </div>
-    );
+        {/* //! Right section */}
+        <div className="w-1/2 h-screen grid place-items-center">
+          <h2 className="text-4xl">Signup Page</h2>
+          <h2 className="text-4xl"> this is where the form will be</h2>
+
+          <p className="inline-flex gap-1 text-base">
+           Already have an account?{" "}
+            <Link href="/login" className="hover:underline flex items-center">
+              Login <ArrowTopRightIcon />
+            </Link>
+          </p>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default SignupPage;
