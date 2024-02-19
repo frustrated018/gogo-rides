e519cda4b107cd48bbb01bfbd5c32493d078a291
frustrated018 <niloyd018@gmail.com>
2024-02-19 23:32:45 +0600

    Successfully created database connection

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -1,5 +1,8 @@
 "use server";
 
+import connect from "@/database/db";
+import User from "@/database/models/user";
+
 export async function createUser(formData) {
   const rawFormData = {
     name: formData.get("name"),
@@ -7,6 +10,18 @@ export async function createUser(formData) {
     password: formData.get("password"),
     photoUrl: formData.get("photoUrl"),
   };
+  console.log("Raw Data: ", rawFormData);
 
-  console.log(rawFormData);
+  try {
+    await connect();
+
+    // TODO: Check if user already exists in DB
+
+    const newUser = new User(rawFormData);
+    await newUser.save();
+
+    console.log("new user:", newUser);
+  } catch (error) {
+    throw error;
+  }
 }
---
+++ database/db.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+const connect = async () => {
+  const connectionState = mongoose.connection.readyState;
+
+  if (connectionState === 1) {
+    console.log("Already connected");
+    return;
+  }
+
+  if (connectionState === 2) {
+    console.log("Connecting...");
+    return;
+  }
+
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      dbName: "Curtains&Drapes_DB",
+      bufferCommands: false,
+      serverSelectionTimeoutMS: 30000,
+    });
+    console.log("Connected to DB");
+  } catch (error) {
+    console.log("Error in connecting to database", error);
+    throw new Error("Error connecting to database");
+  }
+};
+
+export default connect;
