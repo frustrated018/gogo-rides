d55b68d6a05ec14e36f1c9bdcd8e4d0f9b55c5d1
frustrated018 <niloyd018@gmail.com>
2024-02-20 22:05:50 +0600

    Did basic responsiveness for the navbar

--- components/nav-home/navHome.jsx
+++ components/nav-home/navHome.jsx
@@ -3,16 +3,16 @@ import { ThemeToggle } from "@/components/ui/theme-toggle";
 import LoginLogout from "./login-logout";
 import UserAvatar from "./user-avatar";
 
-//TODO: Make this thing responsive
+//TODO: Did basic responsiveness will turn into sheet if time remains
 
 const NavHome = () => {
   return (
     <header className="bg-background border-b">
-      <section className="p-6 flex justify-between items-center">
-        <Link href="/" className="text-xl font-semibold">
+      <section className="p-6 flex flex-col md:flex-row gap-3 md:gap-0 justify-between items-center">
+        <Link href="/" className="text-3xl md:text-xl font-semibold">
           GOGO Rides
         </Link>
-        <div className="flex items-center gap-5">
+        <div className="flex flex-col md:flex-row items-center gap-2 md:gap-5">
           <Link href="/">Home</Link>
           <Link href="/add">Add Product</Link>
           <Link href="/cart">My Cart</Link>
