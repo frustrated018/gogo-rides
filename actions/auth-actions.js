6f03d081817b733e3a44f8d03f34c81e482b7271
frustrated018 <niloyd018@gmail.com>
2024-02-20 16:14:42 +0600

    Login page setup done
    Set up the login page to handle the db call and check if user exists in db...
    Need to fine tune the logic but this will do for now
    Alos added the social login using google

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -27,3 +27,27 @@ export async function createUser(formData) {
     throw new Error("Failed to Add User To the Database", { cause: error });
   }
 }
+
+//? Function to check for user in db are different for signup and login. in one if there is user thats an error other positive
+
+//! Users existance for login
+export async function LoginUserExists(formData) {
+  const email = formData.get("email");
+  try {
+    await connect();
+    const userMatch = await User.findOne({ email });
+
+    // if doesn't exist throw error
+    if (!userMatch) {
+      throw new Error("User doesn't exists");
+    }
+
+    // If user exist, return a success message
+    return "Found user in DB";
+
+    
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+}
--- app/(Auth)/login/login-form.jsx
+++ app/(Auth)/login/login-form.jsx
@@ -1,30 +1,110 @@
+"use client";
+
+import { LoginUserExists } from "@/actions/auth-actions";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { ReloadIcon } from "@radix-ui/react-icons";
+import { useRouter } from "next/navigation";
+import { FaGoogle } from "react-icons/fa6";
+import { toast } from "sonner";
+import { useFormStatus } from "react-dom";
+import {
+  useSignInWithEmailAndPassword,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/config";
+
+//! Submit button based on state
+function SubmitButton() {
+  const router = useRouter();
+  const { pending } = useFormStatus();
+  const [signInWithGoogle] = useSignInWithGoogle(auth);
+
+  return (
+    <div className="flex justify-center items-center gap-2 md:gap-5 flex-col sm:flex-row">
+      {pending ? (
+        <Button disabled>
+          <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+          Please wait
+        </Button>
+      ) : (
+        <Button type="submit">Log in with email</Button>
+      )}
+      <p>Or</p>
+      <Button
+        onClick={async () => {
+          try {
+            await signInWithGoogle();
+            // TODO: make a db call to check if user already exisits
+
+            toast.success("Welcome to Back!");
+            router.push("/");
+          } catch (error) {
+            // TODO: Throw error from db
+            toast.error("Couldn't sign up using Google!");
+          }
+        }}
+        className="flex justify-center items-center gap-2"
+      >
+        <FaGoogle /> Google
+      </Button>
+    </div>
+  );
+}
 
 const LoginForm = () => {
+  const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
+  const router = useRouter();
+
   return (
     <div className="w-full flex flex-col justify-center items-center">
       <h2 className="text-2xl font-semibold mb-5">Login</h2>
-      <form className="w-[90%] max-w-md mx-auto flex-grow space-y-3">
+      <form
+        action={async (formData) => {
+          const password = formData.get("password");
+          const email = formData.get("email");
+          try {
+            const usersExistance = await LoginUserExists(formData);
+
+            const signIn = await signInWithEmailAndPassword(email, password);
+
+            await Promise.all([usersExistance, signIn]);
+
+            // Display success message to the user
+            toast.success("Welcome Back!!");
+            router.push("/");
+          } catch (error) {
+            console.log(error);
+            toast.error(`${error.message}`);
+          }
+        }}
+        className="w-[90%] max-w-md mx-auto flex-grow space-y-3"
+      >
         <div>
           <Label htmlFor="email">Email:</Label>
           <Input
             className="w-full"
-            id="email"
+            name="email"
             placeholder="name@example.com"
             type="email"
             autoCapitalize="none"
             autoComplete="email"
             autoCorrect="off"
+            required
           />
         </div>
 
         <div>
           <Label htmlFor="email">Password:</Label>
-          <Input id="password" placeholder="****************" type="password" />
+          <Input
+            name="password"
+            placeholder="****************"
+            type="password"
+            required
+          />
         </div>
-        <Button type="submit">Log in with Email</Button>
+        <SubmitButton />
       </form>
     </div>
   );
