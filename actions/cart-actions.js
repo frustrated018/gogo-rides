9701554330b1eb943c17f99e0e1f43fa9b984e30
frustrated018 <niloyd018@gmail.com>
2024-02-21 17:09:24 +0600

    Updated user to verified user for better redability :3

--- actions/cart-actions.js
+++ actions/cart-actions.js
@@ -17,8 +17,8 @@ export const addToCart = async (vehicleId, userEmail) => {
     }
 
     // Check if user exists in db by email
-    const user = await User.find({ email: userEmail });
-    if (user.length === 0) {
+    const verifiedUser = await User.find({ email: userEmail });
+    if (verifiedUser.length === 0) {
       throw new Error("User not found in database");
     }
 
@@ -29,14 +29,14 @@ export const addToCart = async (vehicleId, userEmail) => {
     }
 
     // Check if vehicle is in users cart already
-    const cart = await Cart.findOne({ user: user?._id });
+    const cart = await Cart.findOne({ user: verifiedUser?._id });
     if (cart && cart.vehicles.includes(vehicleId)) {
       throw new Error("Vehicle already exists in your cart.");
     }
 
     // If it meets all the conditions add the thing to users cart
     if (!cart) {
-      await Cart.create({ user: user?._id, vehicles: [vehicleId] });
+      await Cart.create({ user: verifiedUser?._id, vehicles: [vehicleId] });
     } else {
       // If the user already has a cart, update it
       cart.vehicles.push(vehicleId);
